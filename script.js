594b21fb8e6ad88a43cb8a0e70a715deeee6b4f6
Brock Atwood <brock.atwood@du.edu>
2021-08-10 22:07:44 -0600

    more changes to html and Javascript, starting over

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
-// var searchBtn = $("#search-btn");
-// var date = "#date";
-// var temp = $("#temp");
-// var humidity = $("#humidity");
-// var wind = $("#wind");
-// var uvIndex = $("#uvIndex");
-// var uvIndexColor = $("#uvIndexColor");
-// var fiveDay = $("#future-forcast");
-// var searchHistoryDiv = $("#search-history");
-// var historyButton = $(".btn-group");
-// var clearnBtn = $("#clear-btn");
-// var cityIcon = $("#cityIcon");
-// var currentWeather = $("#current-weather");
+// const historyKey = "weatherCitySearchHistory";
+// const historyBtnBsClasses = "btn btn-dark border text-left";
+// const historyDataCityAttr = "data-city";
+const API_Key = "714c68372a87fcad429b4ef3a4b1ece0";
 
-// var API_Key = "714c68372a87fcad429b4ef3a4b1ece0";
-// // var location = "boston";
-
-// var searchListHistory = JSON.parse(
-//   localStorage.getItem("searchListHistory") || "[]"
-// );
-
-// var currentDay = moment().format("dddd, MMMM Do YYYY");
-
-// renderHistory();
-
-// if (searchListHistory[0]) {
-//   displayWeather(searchListHistory[searchListHistory.length - 1]);
+// var searchListHistory = JSON.parse(localStorage.getItem(historyKey));
+// if (!searchListHistory) {
+//   searchListHistory = [];
 // }
 
-// function renderHistory() {
-//   searchHistoryDiv.empty();
-//   for (var i = 0; i < searchListHistory.length; i++) {
-//     var newBtn = $("<button>");
-//     newBtn.attr("class", "history-btn");
-//     newBtn.html(searchListHistory[i]);
-//     searchHistoryDiv.prepend(newBtn);
+// $(init);
+
+// function init() {
+//   renderHistory();
+//   $("search-form").then("submit", handleSearch);
+//   $("search-history").JSON("click", handleHistoryItemClick);
+
+//   if (history.length > 0) {
+//     showCityWeather(history[history.length - 1]);
 //   }
 // }
 
@@ -61,42 +44,92 @@
 //     }).then(function (response) {
 //       var uvIndex = response.value;
 //       $("#uvIndex").text(uvIndex);
-//       $("#uvIndexColor").showCityWeather("style", "display: block;");
+//       $("#uvIndex").attr("style", getUVColorStyle(uvIndex));
+//       $("#uvIndexColor").attr("style", "display: block");
 //     });
 
-//     $("#current-weather").text(
-//       response.name + " (" + moment().format("l") + ")"
-//     );
-//     $("#cityIcon").currentWeather(
+//     $("#current-city").text(response.name + " (" + moment().format("l") + ")");
+//     $("#current-icon").currentWeather(
 //       "src",
 //       weatherIconURL(response.weather[0].icon)
 //     );
 //     $("#temp").text(response.main.temp.tofixed(1));
 //     $("#humidity").text(response.main.humidity);
 //     $("#wind").text(response.wind.speed);
+//     $("current-weather").attr("style", "display: block");
 //   });
 
-//   var forcastQueryURL = "https://api.openweathermap.org/data/2.5/forcast?units=imperial&appid=" + API_Key + "&lat=" + city;
+//   var forcastQueryURL =
+//     "https://api.openweathermap.org/data/2.5/forcast?units=imperial&appid=" +
+//     API_Key +
+//     "&lat=" +
+//     city;
 //   $.ajax({
-//       url: forcastQueryURL,
-//       method: "GET",
-//   }).then(function(response) {
-//       var listIndex =
-//   }
+//     url: forcastQueryURL,
+//     method: "GET",
+//   }).then(function (response) {
+//     var listIndex = GoodStartIndex(response);
+//     var list = response.list;
+
+//     for (var i = 1; i <= 5; i++) {
+//       var dayCard = $("#forecast-" + i);
+//       dayCard.find("h5").text(moment(list[listIndex].dt * 1000).format("l"));
+//       dayCard
+//         .find("img")
+//         .attr("src", getWeatherIconURL(list[listIndex].weather[0].icon));
+//       dayCard.find(".temp").text(list[listIndex].main.temp.toFixed(1));
+//       dayCard.find(".humidity").text(list[listIndex].main.humidity);
+//       listIndex += 8;
+//     }
+//     $("#future-forecast").attr("style", "display: block");
+//   });
 // }
 
 // function handleSearch(event) {
-//     event.preventDefault();
-//     var city = $("#city-input").val().trim();
-//     $("#city-input").val("");
-//     addHistoryItem (city);
-//     showCityWeather (city);
+//   event.preventDefault();
+//   var city = $("#search-input").val().trim();
+//   $("#search-input").val("");
+//   addHistoryItem(city);
+//   showCityWeather(city);
 // }
 
 // function handleHistoryItemClick(event) {
-//     if(event.target.matches("button")) {
-//         showCityWeather($(event.target).attr("#data-city"));
-//     }
+//   if (event.target.matches("button")) {
+//     showCityWeather($(event.target).attr(historyDataCityAttr));
+//   }
+// }
+
+// function renderHistory() {
+//   var searchHistory = $("#search-history").empty();
+//   history.forEach((city) => {
+//     var btn = $("<button>").addClass(historyBtnBsClasses);
+//     btn.attr(historyDataCityAttr, city);
+//     btn.text(city);
+//     searchHistory.append(btn);
+//   });
+// }
+
+// function GoodStartIndex(response) {
+//   var list = response.list;
+//   var startIndex = 8;
+//   do {
+//     startIndex--;
+//     indexHour = parseInt(moment(list[startIndex].dt * 1000).format("H"));
+//   } while (indexHour >= 15 && startIndex > 0);
+
+//   return startIndex;
+// }
+
+// function getUVColorStyle(uvIndex) {
+//   if (uvIndex <= 3) {
+//     return "background-color: green; color: white";
+//   } else if (uvIndex <= 7) {
+//     return "background-color: orange; color: white";
+//   } else if (uvIndex <= 10) {
+//     return "background-color: red; color: white";
+//   } else {
+//     return "background-color: purple; color white";
+//   }
 // }
 
 function testRun() {
@@ -122,6 +155,6 @@ function testRun() {
 
 testRun();
 
-// function weatherIconURL(iconCode) {
+// function getweatherIconURL(iconCode) {
 //   return "https://openweathermap.org/img/w/" + iconCode + ".png";
 // }
